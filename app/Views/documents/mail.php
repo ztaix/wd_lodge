152130d2375502dd026fdb8888ac8894266ec29e
Antoine LOPEZ <a.lopez@wayz.digital>
2023-11-06 02:27:05 -1000

    email

--- app/Views/documents/mail.php
+++ app/Views/documents/mail.php
@@ -86,12 +86,11 @@ $due_date = $date->format('d/m/Y');
 
 <body>
 <?php 
-var_dump($data);
 
 ?>
 	<div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
 		<a href="#">
-			<img class="rounded-t-lg" src="<?=$seller[1]['Data'] ?>" alt="" />
+			<img class="rounded-t-lg" style="max-width: 100px ;" src="<?=$seller[1]['Data'] ?>" alt="" />
 		</a>
 		<div class="p-5">
 			<a href="#">
@@ -103,13 +102,8 @@ var_dump($data);
 			<p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Nous sommes très impatient de vous accueillir !</p>
 			<a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
 				Statut du paiement : <?=$data['booking_info']['Paid'] ?> / <?=$data['booking_info']['Price'] ?> Fr
-				<svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-					<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
-				</svg>
 			</a>
-			<p><b>Commentaire de réservation:</b> <br>
-				<?=$data['booking_info']['Comment'];?>
-			</p>
+			<?=$data['booking_info']['Comment']? '<p><b>Commentaire de réservation:</b> <br>'.$data['booking_info']['Comment'].'</p>':'';?>
 		</div>
 	</div>
 
