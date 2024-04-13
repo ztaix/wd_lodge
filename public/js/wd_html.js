791c3c346ed62750de4d4fc1f2d171edc929b7d6
Antoine LOPEZ <a.lopez@wayz.digital>
2024-04-12 23:31:53 -1000

    try fix npm build transcript name cosnt

--- public/js/wd_html.js
+++ public/js/wd_html.js
@@ -946,6 +946,8 @@ if (urlLocation() == 'history') {
 
   function updatePagination() {
     const itemsPerPage = 8;
+    const all_bookings = Array.from(document.querySelectorAll('.ROW'));
+
     let paginationHtml = `<nav class="flex flex-col md:flex-row justify-between items-start md:items-center space-y-3 md:space-y-0 p-4" aria-label="Table navigation">
       <span class="text-sm font-normal text-gray-500 dark:text-gray-400">
           Ligne
